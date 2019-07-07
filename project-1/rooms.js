96be2516485510fdcb7472d3caa34f0c88ba04af
DonC24 <51435215+DonC24@users.noreply.github.com>
2019-07-07 10:49:34 +0800

    Movements between rooms work. Buttons and text box visibility changes

---
+++ project-1/rooms.js
@@ -0,0 +1,53 @@
+let rooms = [{
+    name:"room 1",
+    color: "blue",
+    description: "The room looks like this (array 0",
+    exits: {
+        north: 1
+    }
+},
+{
+    name: "room 2",
+    color: "red",
+    description: "This is room 2 (array 1)",
+    exits: {
+        south: 0,
+        north: 2,
+        west: 4,
+        east: 5
+    }
+},
+{
+    name: "room 3",
+    color: "yellow",
+    description: "This is room 3 (array 2)",
+    exits: {
+        north: 3,
+        south: 1
+    }
+},
+{
+    name: "room 4",
+    color: "orange",
+    description: "This is room 4 (array 3)",
+    exits: {
+        south: 2
+    }
+},
+{
+    name: "room 5",
+    color: "pink",
+    description: "This is room 5 (array 4)",
+    exits: {
+        east: 1
+    }
+},
+{
+    name: "room 6",
+    color: "pink",
+    description: "This is room 6 (array 5)",
+    exits: {
+        west: 1
+    }
+}
+];
---
+++ project-1/script.js
@@ -0,0 +1,123 @@
+console.log("hello script js");
+
+var display = function( data ){
+    var output = document.querySelector('#output');
+    output.innerHTML = data;
+};
+
+var playerName = null;
+var currentRm = 0;
+var playerTurnCount = 0;
+var rmExits = Object.keys(rooms[currentRm].exits);
+// let roomNum = rooms[i];
+var nthBtn = document.getElementById("btn-north");
+var sthBtn = document.getElementById("btn-south");
+var eastBtn = document.getElementById("btn-east");
+var westBtn = document.getElementById("btn-west");
+
+
+if (currentRm === 0 && playerTurnCount === 0){ //just to ensure it is a new game
+    display(`Hello there, what is your name? Welcome to ${rooms[0].name}.`);
+    nthBtn.style.visibility = 'hidden';
+    sthBtn.style.visibility = 'hidden';
+    eastBtn.style.visibility = 'hidden';
+    westBtn.style.visibility = 'hidden';
+}
+
+
+
+
+/*  if (x.style.visibility === 'hidden') {
+    x.style.visibility = 'visible';
+  } else {
+    x.style.visibility = 'hidden';
+};*/
+
+
+
+
+var navVis = function() { //check exits and updates movement buttons
+    console.log('in navVis func, current room is: ' + currentRm);
+    rmExits = Object.keys(rooms[currentRm].exits);
+    if (rmExits.includes("north")){
+        console.log('North btn is visible');
+        console.log("north btn. current room is " + currentRm);
+        nthBtn.style.visibility = 'visible';
+    } else {
+        console.log('North btn is not visible');
+        nthBtn.style.visibility = 'hidden';
+    }
+    if (rmExits.includes("south")){
+        console.log('south btn is visible');
+        sthBtn.style.visibility = 'visible';
+    } else {
+        console.log('south btn is not visible');
+        sthBtn.style.visibility = 'hidden';
+    }
+    if (rmExits.includes("east")){
+        console.log('east btn is visible');
+        eastBtn.style.visibility = 'visible';
+    } else {
+        console.log('east btn is not visible');
+        eastBtn.style.visibility = 'hidden';
+    }
+    if (rmExits.includes("west")){
+        console.log('west btn is visible');
+        westBtn.style.visibility = 'visible';
+    } else {
+        console.log('west btn is not visible');
+        westBtn.style.visibility = 'hidden';
+    }
+};
+
+//movement button event listeners
+nthBtn.addEventListener("click", function(){
+            var newRm = rooms[currentRm].exits.north;
+            currentRm = newRm;
+            display(`${rooms[currentRm].description}`);
+            console.log('current room is: ' + currentRm + ' New room: ' + newRm);
+            navVis();
+            return currentRm;
+        });
+
+sthBtn.addEventListener("click", function(){
+            var newRm = rooms[currentRm].exits.south;
+            currentRm = newRm;
+            display(`${rooms[currentRm].description}`);
+            console.log('current room is: ' + currentRm + ' New room: ' + newRm);
+            navVis();
+            return currentRm;
+        });
+
+eastBtn.addEventListener("click", function(){
+            var newRm = rooms[currentRm].exits.east;
+            currentRm = newRm;
+            display(`${rooms[currentRm].description}`);
+            console.log('current room is: ' + currentRm + ' New room: ' + newRm);
+            navVis();
+            return currentRm;
+        });
+
+westBtn.addEventListener("click", function(){
+            var newRm = rooms[currentRm].exits.west;
+            currentRm = newRm;
+            display(`${rooms[currentRm].description}`);
+            console.log('current room is: ' + currentRm + ' New room: ' + newRm);
+            navVis();
+            return currentRm;
+        });
+
+document.getElementById("submit-btn").addEventListener("click", function(){ //first run only. waiting for player to input name
+    var inputResults = document.getElementById('inputtxt').value;
+    console.log("input value: " + inputResults);
+    while(playerName === null){
+        playerName = inputResults;
+        console.log("player name: " + playerName);
+    }
+    display(`${rooms[currentRm].description}`);
+    document.getElementById("submit-btn").style.visibility = 'hidden';
+    document.getElementById("inputtxt").style.visibility = 'hidden';
+    currentRm = 0;
+    navVis();
+    playerTurnCount++;
+});
